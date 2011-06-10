34630b8390a45e524970e7c5dfc7cefe2795ff51
maeluiseryn <maeluiseryn@maeluiseryn-desktop.(none)>
2011-06-10 17:47:19 +0200

    invoice: no new invoice link if remaining ==0

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,12 @@
 module InvoicesHelper
+  def project_remaining_to_pay(project)
+    project.project_price-get_remaining_to_pay(project)
+  end
+  def project_remaining_to_pay_equal_zero(project)
+    if project_remaining_to_pay(project)<=0
+      true
+    else
+      false
+    end
+  end
 end
